79be239c53ab0bcccb9cd0b9598083877f4aff53
Carlos <carlosbolorino@hotmail.com>
2023-04-09 16:20:26 +0200

    add modules routing

--- 03-adminpro/src/app/app-routing.module.ts
+++ 03-adminpro/src/app/app-routing.module.ts
@@ -1,37 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graph1Component } from './pages/graph1/graph1.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+import { AuthRoutingModule } from './auth/auth.routing';
+
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { 
-    path: '', 
-    component: PagesComponent,
-    children: [
-      {path: 'dashboard', component: DashboardComponent},
-      {path: 'progress', component: ProgressComponent},
-      {path: 'graph1', component: Graph1Component},
-      {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  },
-  {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '**', component: NopagefoundComponent},
+  //path: /dashboard => PagesRouting
+  //path: /auth => AuthRouting 
+  { path: '**', component: NopagefoundComponent },
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
-  exports: [ RouterModule ]
+  exports: [RouterModule]
 })
 
 export class AppRoutingModule {
 
- }
+}
